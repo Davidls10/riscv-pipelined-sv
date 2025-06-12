cd67d530e1f6ad38ec3a807e9f0def15e3255083
David <104114592+Davidls10@users.noreply.github.com>
2025-06-12 13:06:05 -0300

    Update register_file.sv

--- processor/register_file.sv
+++ processor/register_file.sv
@@ -7,16 +7,8 @@ module register_file #(parameter N = 5,  M = 32, L = 32)
 
     logic [M-1:0] rf [L-1:0];
 
-    initial begin
-        rf[9] = 32'h4;
-        rf[4] = 32'd14;
-        rf[6] = 32'd5;
-        rf[1] = 32'd2;
-    end
-
     always_ff @(negedge clk) begin
             if (we) rf[a3] = wd3;
-        $display("rf[4] = %d  rf[5] = %d  rf[6] = %d  rf[9] = %d\n", rf[4], rf[5], rf[6], rf[9]);
     end
 
     assign rd1 = (a1 != 0) ? rf[a1] : 0;
