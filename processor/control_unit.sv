c3e88b2e198173d706d48972d28b014db164d846
David <104114592+Davidls10@users.noreply.github.com>
2025-06-12 11:12:08 -0300

    Update control_unit.sv

--- processor/control_unit.sv
+++ processor/control_unit.sv
@@ -44,107 +44,85 @@ module main_decoder (input logic [7-1:0] opcode,
                      output logic jump
                     );
 
-    /*initial begin
-        reg_write <= 1'b0;
-        imm_src <= 2'b00;
-        alu_src <= 1'b0;
-        mem_write <= 1'b0;
-        result_src <= 2'b00;
-        branch <= 1'b0;
-        alu_op <= 2'b00;
-        jump <= 1'b0;        
-    end*/
-
     always_comb begin
         case (opcode)
             7'b0000011: // lw
                 begin
-                    reg_write <= 1'b1;
-                    imm_src <= 2'b00;
-                    alu_src <= 1'b1;
-                    mem_write <= 1'b0;
-                    result_src <= 2'b01;
-                    branch <= 1'b0;
-                    alu_op <= 2'b00;
-                    jump <= 1'b0;
+                    reg_write = 1'b1;
+                    imm_src = 2'b00;
+                    alu_src = 1'b1;
+                    mem_write = 1'b0;
+                    result_src = 2'b01;
+                    branch = 1'b0;
+                    alu_op = 2'b00;
+                    jump = 1'b0;
                 end
             7'b0100011: // sw
                 begin
-                    reg_write <= 1'b0;
-                    imm_src <= 2'b01;
-                    alu_src <= 1'b1;
-                    mem_write <= 1'b1;
-                    result_src <= 2'bxx;
-                    branch <= 1'b0;
-                    alu_op <= 2'b00;
-                    jump <= 1'b0;
+                    reg_write = 1'b0;
+                    imm_src = 2'b01;
+                    alu_src = 1'b1;
+                    mem_write = 1'b1;
+                    result_src = 2'bxx;
+                    branch = 1'b0;
+                    alu_op = 2'b00;
+                    jump = 1'b0;
                 end
-            7'b0110011: // R-type or
+            7'b0110011: // R-type instructions
                 begin
-                    reg_write <= 1'b1;
-                    imm_src <= 2'bxx;
-                    alu_src <= 1'b0;
-                    mem_write <= 1'b0;
-                    result_src <= 2'b00;
-                    branch <= 1'b0;
-                    alu_op <= 2'b10;
-                    jump <= 1'b0;
-                end
-            7'b0110011: // R-type and
-                begin
-                    reg_write <= 1'b1;
-                    imm_src <= 2'bxx;
-                    alu_src <= 1'b0;
-                    mem_write <= 1'b0;
-                    result_src <= 2'b00;
-                    branch <= 1'b0;
-                    alu_op <= 2'b10;
-                    jump <= 1'b0;
+                    reg_write = 1'b1;
+                    imm_src = 2'bxx;
+                    alu_src = 1'b0;
+                    mem_write = 1'b0;
+                    result_src = 2'b00;
+                    branch = 1'b0;
+                    alu_op = 2'b10;
+                    jump = 1'b0;
                 end
             7'b1100011: // beq
                 begin
-                    reg_write <= 1'b0;
-                    imm_src <= 2'b10;
-                    alu_src <= 1'b0;
-                    mem_write <= 1'b0;
-                    result_src <= 2'bxx;
-                    branch <= 1'b1;
-                    alu_op <= 2'b01;
-                    jump <= 1'b0;
+                    reg_write = 1'b0;
+                    imm_src = 2'b10;
+                    alu_src = 1'b0;
+                    mem_write = 1'b0;
+                    result_src = 2'bxx;
+                    branch = 1'b1;
+                    alu_op = 2'b01;
+                    jump = 1'b0;
                 end
             7'b0010011: // addi
                 begin
-                    reg_write <= 1'b1;
-                    imm_src <= 2'b00;
-                    alu_src <= 1'b1;
-                    mem_write <= 1'b0;
-                    result_src <= 2'b00;
-                    branch <= 1'b0;
-                    alu_op <= 2'b10;
-                    jump <= 1'b0;
+                    reg_write = 1'b1;
+                    imm_src = 2'b00;
+                    alu_src = 1'b1;
+                    mem_write = 1'b0;
+                    result_src = 2'b00;
+                    branch = 1'b0;
+                    alu_op = 2'b10;
+                    jump = 1'b0;
                 end
             7'b1101111: // jal
                 begin
-                    reg_write <= 1'b1;
-                    imm_src <= 2'b00;
-                    alu_src <= 1'b1;
-                    mem_write <= 1'b0;
-                    result_src <= 2'b10;
-                    branch <= 1'b0;
-                    alu_op <= 2'bxx;
-                    jump <= 1'b1;
+                    reg_write = 1'b1;
+                    imm_src = 2'b00;
+                    alu_src = 1'b1;
+                    mem_write = 1'b0;
+                    result_src = 2'b10;
+                    branch = 1'b0;
+                    alu_op = 2'bxx;
+                    jump = 1'b1;
                 end
 
             default:
                 begin
-                    reg_write <= 1'bx;
-                    imm_src <= 2'bxx;
-                    alu_src <= 1'bx;
-                    mem_write <= 1'bx;
-                    result_src <= 2'bxx;
-                    branch <= 1'bx;
-                    alu_op <= 2'bxx;
-                    jump <= 1'bx;   
+                    reg_write = 1'bx;
+                    imm_src = 2'bxx;
+                    alu_src = 1'bx;
+                    mem_write = 1'bx;
+                    result_src = 2'bxx;
+                    branch = 1'bx;
+                    alu_op = 2'bxx;
+                    jump = 1'bx;   
                 end
         endcase
     end
